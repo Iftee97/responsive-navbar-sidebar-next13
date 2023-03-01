fefb8eed2fb2cf4fe79514edff1ba446c25ce310
Iftee97 <tawsifhaque97@gmail.com>
2023-03-01 18:57:44 +0600

    update logo

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
       <main className="max-w-[1440px] mx-auto px-[30px] py-[60px] md:pt-[30px] md:pb-0">
         {children}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
